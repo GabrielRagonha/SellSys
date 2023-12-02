c3fe67ce02adc2b8bfb11e20be1823a2809f8793
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2023-12-02 15:44:34 -0300

    feat: create sales view

---
+++ resources/views/sale/create.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Inserir nova venda') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    <form action="{{ route('sale.store') }}" method="post">
+                        @csrf
+                        <select name="id_seller" required>
+                            @foreach ($sellers as $key => $seller)
+                                <option value="{{ $seller }}">{{ $key }}</option>
+                            @endforeach
+                        </select>
+
+                        <input name="sale_value" type="number" required>
+
+                        <button type="submit">Criar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
